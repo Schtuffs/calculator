6d84ad8ceb5809a551bdd522a1038ff93f2f8c5a
Schtuffs <kwagler7621@gmail.com>
2026-01-07 00:00:39 -0500

    moved token code to own file

--- src/main.rs
+++ src/main.rs
@@ -18,36 +18,8 @@
 
 use std::io::{self, Write};
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-enum TokenType {
-    Number,
-    Addition,
-    Subtract,
-    Multiply,
-    Division,
-    Exponent,
-    ParenOpen,
-    ParenClose,
-}
-
-#[allow(non_snake_case)]
-mod TokenPrio {
-    pub const NONE: i8  = 0;
-    pub const ADD: i8   = 1;
-    pub const SUB: i8   = 1;
-    pub const MUL: i8   = 2;
-    pub const DIV: i8   = 2;
-    pub const EXP: i8   = 3;
-    pub const PAR: i8   = 4;
-    pub const MAX: i8   = 4;
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-struct Token {
-    value: String,
-    ttype: TokenType,
-    prio: i8,
-}
+mod token;
+use token::{Token, TokenType, TokenPrio};
 
 fn main() {
     println!("(Negative numbers should be enclosed with brackets for proper formatting)");
